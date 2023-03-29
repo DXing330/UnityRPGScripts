8558bffc0b5c798f844815e031e1e98edd1a6e97
Briky <draconyth@gmail.com>
2023-03-29 13:11:03 -0700

    Made is possible for NPCs to say multiple lines.

--- NPCText.cs
+++ NPCText.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class NPCText : Collideable
 {
-    public string text;
-    private float last_text = -4.0f;
-    private float text_cooldown = 4.0f;
+    public List<string> texts;
+    private int last_text_shown = 0;
+    public float text_cooldown = 4.0f;
+    private float last_text_shown_time;
+
+    protected override void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        last_text_shown_time = -text_cooldown;
+    }
 
     protected override void OnCollide(Collider2D coll)
     {
-        if (Time.time - last_text > text_cooldown && coll.name == "Player")
+        if (Time.time - last_text_shown_time > text_cooldown && coll.name == "Player" && texts.Count > 0)
         {
-            last_text = Time.time;
-            GameManager.instance.ShowText(text, 15, Color.white, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, text_cooldown);
+            last_text_shown_time = Time.time;
+            GameManager.instance.ShowText(texts[last_text_shown], 15, Color.white, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, text_cooldown);
+            if (last_text_shown < texts.Count - 1)
+            {
+                last_text_shown++;
+            }
+            else
+            {
+                last_text_shown = 0;
+            }
         }
     }
 }
